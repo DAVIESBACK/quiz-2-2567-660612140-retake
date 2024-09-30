7229440cd276c1da97ad991079699d46a276926e
win <wwinnn478@gmail.com>
2024-09-30 16:51:11 +0700

    !fiish

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,5 +1,26 @@
 // You can define interfaces for all React components here.
 // Do not forget to export properly.
+interface postOwnerProps {
+    username: string;
+    userImagePath: string;
+    postText: string;
+    likeNum: number;
+}
+export type { postOwnerProps };
 
-interface ReplyProps {}
+interface commentProps {
+    username: string;
+    userImagePath: string;
+    commentText: string;
+    likeNum: number;
+    replies: ReplyProps;
+}
+export type { commentProps };
+
+interface ReplyProps {
+    username: string;
+    userImagePath: string;
+    replyText: string;
+    likeNum: number;
+}
 export type { ReplyProps };
